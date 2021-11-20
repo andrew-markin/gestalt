fcb149ef418042f344d3198884bad95a51b7896c
Andrew N. Markin <andrew.n.markin@gmail.com>
2021-11-20 23:24:32 +0300

    UI design of tasks dragging slightly modified

--- src/views/TaskBoard.vue
+++ src/views/TaskBoard.vue
@@ -7,7 +7,7 @@
       <template #item="{ item: task }">
         <drag
           :key="task.uuid"
-          :drag-image-opacity="0.5">
+          :drag-image-opacity="0.9">
           <v-sheet
             class="task rounded pa-4 my-3 d-flex align-center noselect"
             @click="openTask(task.uuid)">
@@ -15,7 +15,17 @@
               {{ task.data.description }}
             </span>
           </v-sheet>
+          <template #drag-image>
+            <v-sheet dark class="dragged rounded indigo pa-2 d-flex align-center noselect">
+              <span class="text-body-1 text-truncate">
+                {{ task.data.description }}
+              </span>
+            </v-sheet>
+          </template>
         </drag>
+      </template>
+      <template #reordering-feedback>
+        <div class="feedback" key="feedback"/>
       </template>
     </drop-list>
   </v-container>
@@ -36,7 +46,15 @@ export default {
 </script>
 
 <style scoped>
-.task:hover {
-  background-color: #fafeff;
+.dragged {
+  max-width: 20rem;
+  transform: translate(-4rem, -50%);
+}
+.feedback {
+  outline: 1px solid #3F51B5;
+  position: relative;
+  top: -6px;
+  margin-left: -0.5rem;
+  margin-right: -0.5rem;
 }
 </style>
