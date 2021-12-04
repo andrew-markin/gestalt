74001d5d82363229119f91b9401817c4b7c857bb
Andrew N. Markin <andrew.n.markin@gmail.com>
2021-12-04 23:10:49 +0300

    Dragging into tasks outlined for future implementation

--- src/views/TaskBoard.vue
+++ src/views/TaskBoard.vue
@@ -11,6 +11,10 @@
           <v-sheet
             class="task rounded pa-4 my-3 d-flex align-center noselect"
             @click="openTask(task.uuid)">
+            <drop
+              class="drop-area"
+              @drop="appendChild">
+            </drop>
             <span class="mr-2 text-body-1 text-truncate">
               {{ task.data.description }}
             </span>
@@ -32,15 +36,18 @@
 </template>
 
 <script>
-import { Drag, DropList } from 'vue-easy-dnd'
+import { Drag, Drop, DropList } from 'vue-easy-dnd'
 import { mapState, mapActions, mapMutations } from 'vuex'
 
 export default {
-  components: { Drag, DropList },
+  components: { Drag, Drop, DropList },
   computed: mapState(['tasks']),
   methods: {
     ...mapActions(['reorderTask']),
-    ...mapMutations(['openTask'])
+    ...mapMutations(['openTask']),
+    appendChild (event) {
+      console.log('Append child', event)
+    }
   }
 }
 </script>
@@ -51,10 +58,40 @@ export default {
   transform: translate(-4rem, -50%);
 }
 .feedback {
-  outline: 1px solid #3F51B5;
   position: relative;
-  top: -6px;
+}
+.feedback::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin-top: -7px;
   margin-left: -0.5rem;
   margin-right: -0.5rem;
+  border-top: 2px dashed #3F51B5;
+}
+.task {
+  position: relative;
+}
+.task > .drop-area {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 20px;
+}
+.drop-in::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: -25px;
+  border: 2px dashed #3F51B5;
+  border-radius: 6px;
 }
 </style>
